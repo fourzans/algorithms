5fc003a7164c1b41274174fd2e6091b3e074a75d
mfourzans <mfourzans@sistemasesco.com>
2018-08-29 10:02:49 -0300

    First Commit

---
+++ Algorithms.Classes/BinarySearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Algorithms.Classes
+{
+    public class BinarySearch
+    {
+        public static bool SearchRecursive(int[] array, int x, int left, int right)
+        {
+            if (left > right)
+            {
+                return false;
+            }
+
+            int midPoint = left + (left + right) / 2;
+            if (array[midPoint] == x)
+            {
+                return true;
+            }
+            else if (x < array[midPoint])
+            {
+                return SearchRecursive(array, x, left, midPoint - 1);
+            }
+            else
+            {
+                return SearchRecursive(array, x, midPoint + 1, right);
+            }
+        }
+
+        public static bool SarchIterative(int[] array, int x)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int midPoint = left + (left + right) / 2;
+
+                if (array[midPoint] == x)
+                {
+                    return true;
+                }
+                else if (x < array[midPoint])
+                {
+                    right = midPoint - 1;
+                }
+                else
+                {
+                    left = midPoint + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
